549496d7a3269e029ebe689d873e0501ea910bae
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-10-04 14:28:11 -0500

    Successful CMake build

--- common/controls.hpp
+++ common/controls.hpp
@@ -1,6 +1,11 @@
 #ifndef CONTROLS_HPP
 #define CONTROLS_HPP
 
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+static GLFWwindow* window;
+
 void computeMatricesFromInputs();
 glm::mat4 getViewMatrix();
 glm::mat4 getProjectionMatrix();
